b71777edc49fe79f27cb62f9885b673a887f9a64
coffnes <coffnes@yandex.ru>
2024-02-06 18:52:55 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use bevy::{ecs::query, prelude::*};
+
+fn main() {
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn hello_world() {
+    println!("Hello World!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Home".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
