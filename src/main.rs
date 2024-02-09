c786e13caa2e461bfbc0a4eef118f97cbd2c8a4d
coffnes <coffnes@yandex.ru>
2024-02-09 22:02:28 +0300

    Make code refact

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn(SpriteBundle {
         texture: asset_server.load("ducky.png"),
-            ..Default::default()
+        ..Default::default()
     });
 }
